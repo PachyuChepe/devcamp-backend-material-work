69237695bf439eaf3c37d17ff42029654cbefd22
PachyuChepe <pachyuchepe@gmail.com>
2024-04-24 10:10:34 +0900

    [Feat] dev: .env 환경변수 관리를 위한 @nestjs/config 설치 및 HTTPS 세팅
    - 프로젝트 루트 디렉토리에 가상 인증서가 존재 할 경우 HTTPS로 실행되며, 없을 경우 HTTP로 실행

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
+import * as fs from 'fs';
+import * as https from 'https';
+import * as path from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  // 기본 포트 설정
+  const port = configService.get<number>('SERVER_PORT') || 3000;
+
+  // 인증 키 파일 경로 설정
+  const keyPath = path.join(__dirname, '..', 'key.pem');
+  const certPath = path.join(__dirname, '..', 'cert.pem');
+
+  // HTTPS 설정
+  if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
+    const httpsOptions = {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    };
+    await app.init();
+    https
+      .createServer(httpsOptions, app.getHttpAdapter().getInstance())
+      .listen(port);
+    console.log(`HTTPS server running on port ${port}`);
+  } else {
+    // 인증서가 없을 경우 HTTP로 실행
+    await app.listen(port);
+    console.log(`HTTP server running on port ${port}`);
+  }
 }
+
 bootstrap();
